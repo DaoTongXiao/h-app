6a44139e66205509ab67cde855463c7ea2865a8e
haifeng.zhang <it_dog_zhang@163.com>
2025-08-26 13:37:32 +0800

    set project vue version is 2.x

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 import Vue from 'vue'
 import uView from 'uview-ui'
 import App from './App'
-// #ifndef VUE3
 import './uni.promisify.adaptor'
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.use(uView)
+
 const app = new Vue({
   ...App
 })
 
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
