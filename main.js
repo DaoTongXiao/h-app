666aaf425c5c8545f3f31a78bfa55a23a0c6e33a
haifeng.zhang <it_dog_zhang@163.com>
2025-08-26 13:33:13 +0800

    use uview

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-import App from './App'
-
-// #ifndef VUE3
 import Vue from 'vue'
+import uView from 'uview-ui'
+import App from './App'
+// #ifndef VUE3
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
+Vue.use(uView)
 const app = new Vue({
   ...App
 })
+
 app.$mount()
 // #endif
 
