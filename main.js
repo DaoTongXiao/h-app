98726666720c3520510e5f6da821f3cf105c3784
haifeng.zhang <it_dog_zhang@163.com>
2025-08-26 13:48:48 +0800

    set defalut config

--- main.js
+++ main.js
@@ -6,7 +6,18 @@ import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 Vue.use(uView)
+// set config
+uni.$u.setConfig({
+	config: {
+		unit: 'rpx'
+	},
+	props: {
 
+		radio: {
+			size: 15
+		}
+	}
+})
 const app = new Vue({
   ...App
 })
